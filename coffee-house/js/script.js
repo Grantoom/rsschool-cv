a1f9e8e1061056300b98dd8bd284d8aca04e225e
Maxim Kovalev <wolverin12@ya.ru>
2023-11-29 17:25:56 +0500

    fix: fixed after review

--- coffee-house/js/script.js
+++ coffee-house/js/script.js
@@ -1,17 +1,17 @@
-const coffeeCup = document.querySelector('.header__button');
-const enjoyBtn = document.querySelector('.enjoy__button');
-const mainPage = document.querySelector('.header__link');
+// const coffeeCup = document.querySelector('.header__button');
+// const enjoyBtn = document.querySelector('.enjoy__button');
+// const mainPage = document.querySelector('.header__link');
 
-function openMenu() {
-    document.getElementById('main').hidden = true;
-    document.getElementById('menu').hidden = false;
-}
+// function openMenu() {
+//     document.getElementById('main').hidden = true;
+//     document.getElementById('menu').hidden = false;
+// }
 
-function openMain() {
-    document.getElementById('main').hidden = false;
-    document.getElementById('menu').hidden = true;
-}
+// function openMain() {
+//     document.getElementById('main').hidden = false;
+//     document.getElementById('menu').hidden = true;
+// }
 
-coffeeCup.addEventListener('click', openMenu);
-enjoyBtn.addEventListener('click', openMenu);
-mainPage.addEventListener('click', openMain);
+// coffeeCup.addEventListener('click', openMenu);
+// enjoyBtn.addEventListener('click', openMenu);
+// mainPage.addEventListener('click', openMain);
